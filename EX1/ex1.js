63f52f681afbdf7b05ad4b81372b7aa0bab5ef6f
Anastasios Eleftheriadis <93206130+anEleftheriadis@users.noreply.github.com>
2022-06-27 00:45:11 +0300

    Create ex1.js

---
+++ EX1/ex1.js
@@ -0,0 +1,27 @@
+const inputs = document.getElementsByTagName('input');
+const operator = document.querySelector('select');
+const result = document.querySelector('span');
+
+document.getElementById('calculate').addEventListener('click', function (ev) {
+    let message = 'Invalid numbers...'; 
+
+    const num0 = Number(inputs[0].value);
+    const num1 = Number(inputs[1].value);
+
+    switch (operator.value) {
+        case '+':
+            message = num0 + num1 + '';
+            break;
+        case '-':
+            message = num0 - num1 + '';
+            break;
+        case '*':
+            message = num0 * num1 + '';
+            break;
+        case '/':
+            message = num0 / num1 + '';
+            break;
+    }
+
+    result.innerHTML = message;
+});
