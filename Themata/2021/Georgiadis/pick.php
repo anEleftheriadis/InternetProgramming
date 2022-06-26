c5c01e2522e23b0f92e6fe5470b7f4581c1795ca
Anastasios Eleftheriadis <93206130+anEleftheriadis@users.noreply.github.com>
2022-06-27 00:54:59 +0300

    Update pick.php

--- Themata/2021/Georgiadis/pick.php
+++ Themata/2021/Georgiadis/pick.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "dai17098";
+$dbname = "ics22135";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,14 +11,14 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id FROM dai17098";
+$sql = "SELECT id FROM ics22135";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    if ($result = 17098) {
-        echo "ID (ends with 17098) : " . $row["id"]. "<br>";
+    if ($result = 22135) {
+        echo "ID (ends with 22135) : " . $row["id"]. "<br>";
     }
   }
 } else {
@@ -31,11 +31,11 @@ $conn->close();
 <html>
 <head>
     <title>PHP Εξέταση</title>
-	<link rel="stylesheet" href="dai17098.css"/>
+	<link rel="stylesheet" href="ics22135.css"/>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="description" content="Web Development - September 20201 - PHP">
-    <meta name="dai17098" content="">
+    <meta name="description" content="Web Development - June 2022 - PHP">
+    <meta name="ics22135" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
 </head>
 <body>
